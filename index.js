6149d89e37ddf95c2dd3f98924c9cdc593736869
Wellington Wagner F. Sarmento <wwagner@virtual.ufc.br>
2021-08-10 22:56:03 -0300

    Adicionadas rota para o update e remove. Adicionada funcao update para fazer a atualizacao de dados de cadastro.

--- index.js
+++ index.js
@@ -14,14 +14,25 @@ const express = require("express");
 const app = express();
 const routes = require("./routes");
 const expressLayouts = require("express-ejs-layouts");
+const { urlencoded } = require("express");
 
 const port=3030;
 const address = "localhost";
+
+// a palavra reservada global cria uma variável ou objeto global para o sistemas. Ele pode ser visto em qualquer parta do código ou dos módulos do projeto. Assim, Users podem ser vistos tanto aqui no index.js quanto em routes.js
+global.users =[
+    {name:"Wellington W. F. Sarmento",address:"Rua Dom Jeronimo, 666",email:"wwagner@virtual.ufc.br",age:46,height:1.70,vote:true},
+    {name:"PAtricia S. Paula",address:"Rua Dom Jeronimo, 666",email:"patricia@virtual.ufc.br",age:46,height:1.70,vote:true},
+    {name:"Henrique Sérgio L. Pequeno",address:"Rua do Henrique, 666",email:"henrique@virtual.ufc.br",age:46,height:1.70,vote:true}];
+
 
 
 //ativa uso do EJS e do Express-ejs-layouts
 app.set('view engine','ejs');
 app.use(expressLayouts);
+
+app.use(express.urlencoded({extended:false})); //prepara a aplicacao para receber dados na forma de query string
+app.use(express.json()); //prepara a aplicacao para receber dados no formato JSON
 
 //Criando usando rotas simples que estão no arquivo routes.js
 app.use('/',routes);
